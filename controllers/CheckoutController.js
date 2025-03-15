46c5e50f9324ad69b625b49a7d11f3981d73f7ed
leonardovaloess <leonardoberlandadevaloes@gmail.com>
2025-03-15 01:55:37 -0300

    fix

--- controllers/CheckoutController.js
+++ controllers/CheckoutController.js
@@ -2,28 +2,8 @@ const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 const crypto = require("crypto");
 
-//  Se for fzr gasto por mes tera 2 telas umas pros gastos gerais e outra tela pros gastos
-// mensais. com 2 rotas uma /mes outra /total
-
 class CheckoutController {
   async compraAprovada(req) {
-    /*
-
-     |      created_at: '2025-03-14 16:48:13',
-0|main  |   customer: {
-0|main  |     name: 'João da Silva',
-0|main  |     document: '23875090127',
-0|main  |     email: 'exemplo@email.com',
-0|main  |     phone_number: '5511987654321'
-0|main  |   },
-0|main  |   payment: {
-0|main  |     method: 'CREDIT_CARD',
-0|main  |     brand: 'visa',
-0|main  |     installments: 1,
-0|main  |     finished_at: '2025-03-14 16:48:28'
-0|main  |   },
-
-     */
     console.log(req);
 
     const { customer, products } = req;
